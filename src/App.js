2fd773ff84ec194a29e931b0f43a9cf3983efd7c
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-13 16:59:23 +0200

    Add loading screen

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import List from "./components/List";
 import ListItemIcon from "./components/ListItemIcon";
 import ListItemText from "./components/ListItemText";
 import ListItem from "./components/ListItem";
-
-const bulbasaur = {
-  name: "Bulbasaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
-  id: "001",
-  link: "#bulbasaur",
-};
-const ivysaur = {
-  name: "Ivysaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
-  id: "002",
-  link: "#ivysaur",
-};
-const pokemons = [bulbasaur, ivysaur];
+import { fetchPokemons } from "./api/pokemonApi";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
+  const [pokemons, setPokemons] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function setPokemon() {
+      setLoading(true);
+
+      const allPokemons = await fetchPokemons();
+      setPokemons(allPokemons);
+      setLoading(false);
+    }
+    setPokemon();
+  }, []);
+
   return (
     <div className="app">
       <header className="app__header">
-        <span>Pokedex </span>
+        <span>Pokedex</span>
         <input className="app__searchBar" placeholder="input" />
       </header>
       <main className="app__pokeList colorful-border">
         <List>
-          {pokemons.map((pokemon) => (
+          {loading && <LoadingScreen />}
+
+          {pokemons?.map((pokemon) => (
             <ListItem key={pokemon.id} href={pokemon.link}>
               <ListItemIcon imgSrc={pokemon.imgSrc} altText={pokemon.name} />
               <ListItemText primary={pokemon.name} secondary={pokemon.id} />
---
+++ src/api/pokemonApi.js
@@ -0,0 +1,15 @@
+export async function fetchPokemons() {
+  const response = await fetch("https://unpkg.com/pokemons/pokemons.json");
+  if (!response.ok) {
+    throw new Error(response);
+  }
+  const data = await response.json();
+
+  const pokemons = data.results.map((pokemon) => ({
+    name: pokemon.name,
+    id: pokemon.national_number,
+    imgSrc: pokemon.sprites.large,
+    link: `#${pokemon.name.toLowerCase()}`,
+  }));
+  return pokemons;
+}
